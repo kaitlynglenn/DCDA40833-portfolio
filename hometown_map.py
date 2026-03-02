7d86498138580e4c35003692f5e4665c3ee71c48
kaitlynglenn <kaitlyn.glenn@tcu.edu>
2026-03-01 19:58:57 -0600

    lab 6

---
+++ hometown_map.py
@@ -0,0 +1,9 @@
+pip install folium pandas requests
+import folium   
+import pandas as pd
+import requests
+pip install pandas
+import pandas as pd
+
+df = pd.read_csv("hometown_locations.csv")
+print(df)
